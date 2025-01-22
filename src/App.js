17a2ee51bd247ebca11f7334c2b4faa0eca0515c
arpita244 <arpitatiwari8756@gmail.com>
2025-01-22 22:14:43 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [welcomeMessage, setWelcomeMessage] = useState('');
+
+    useEffect(() => {
+        // Fetch the welcome message from the backend
+        fetch('http://localhost:5000/welcome')
+            .then((response) => response.json())
+            .then((data) => {
+                setWelcomeMessage(data.message);
+            })
+            .catch((error) => {
+                console.error('Error fetching welcome message:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>{welcomeMessage}</h1>
+        </div>
+    );
 }
 
 export default App;
