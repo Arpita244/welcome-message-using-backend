e9dcefdfaddac3a8f9d1aff5b22dcf6835ecd751
arpita244 <arpitatiwari8756@gmail.com>
2025-01-22 23:05:38 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 function App() {
     const [welcomeMessage, setWelcomeMessage] = useState('');
     useEffect(() => {
-        fetch('http://localhost:5000/welcome')
-            .then((response) => response.json())
-            .then((data) => {
+      async function arpi() {
+        const response = await axios.get('http://localhost:5000/welcome');
                 setWelcomeMessage(data.message);
-            })
-            .catch((error) => {
-                console.error('Error fetching welcome message:', error);
-            });
+            }
     }, []);
+
     return (
         <div>
             <h1>{welcomeMessage}</h1>
